e487fd91e9554b08cc7c53e45525383e29b5d4ad
songsc <songscijnfan@hotmail.com>
2018-02-26 06:06:35 -0500

    Changed states

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class HeaderBar extends Component {
+class HeaderBar extends Component {    
     render() {
         return (
                 <div className="HeaderBar">
                     <header className="App-header">
                         <h2 className="Title-Menu">
-                            Click on One of the Options Below
+                            Calculators:
                         </h2>
-                        <h1 className="Title-PV" onClick={this.props.click_PV}>
+                        <h1 className="Title-Calc" 
+                            onClick={this.props.click_PV}>
                             Present Value to Future Value
                         </h1>
-                        <h1 className="Title-AV" onClick={this.props.click_AV}>
+                        <h1 className="Title-Calc" 
+                            onClick={this.props.click_AV}>
                             Annuity to Future Value
                         </h1>
                     </header>
@@ -26,29 +28,9 @@ class Calculator extends Component {
         const choice = this.props.choice;
         switch(choice) {
             case "PV":
-                return(<CalculatorPV
-                            pv={this.props.pv}
-                            n={this.props.n}
-                            r={this.props.r}
-                            fv={this.props.fv}
-                            handle_pv={this.props.handle_pv}
-                            handle_n={this.props.handle_n}
-                            handle_r={this.props.handle_r}
-                            PV_Submit={this.props.PV_Submit}
-                        />);
-                break;
+                return(<CalculatorPV />);
             case "AV":
-                return(<CalculatorAV
-                            av={this.props.av}
-                            n={this.props.n}
-                            r={this.props.r}
-                            fv={this.props.fv}
-                            handle_av={this.props.handle_av}
-                            handle_n={this.props.handle_n}
-                            handle_r={this.props.handle_r}
-                            AV_Submit={this.props.AV_Submit}
-                        />);
-                break;
+                return(<CalculatorAV />);
             default:
                 return(null);
         }
@@ -58,7 +40,12 @@ class Calculator extends Component {
 class CalculatorPV extends Component {
     constructor() {
         super();
-
+        this.state = {
+            pv: 0.0,
+            n: 0,
+            r: 0.0,
+            fv: 0.0,
+        };
         this.pv_change = this.pv_change.bind(this);
         this.n_change = this.n_change.bind(this);
         this.r_change = this.r_change.bind(this);
@@ -66,51 +53,59 @@ class CalculatorPV extends Component {
     }
     
     pv_change(event) {        
-        this.props.handle_pv(event.target.value);
+        this.setState({pv: event.target.value});
     }
 
     n_change(event) {
-        this.props.handle_n(event.target.value);
+        this.setState({n: event.target.value});
     }
     
     r_change(event) {
-        this.props.handle_r(event.target.value);
+        this.setState({r: event.target.value});
     }
     
     PV_Submit(event) {
-        this.props.PV_Submit();
+        const pv = parseFloat(this.state.pv);
+        const r = parseFloat(this.state.r);
+        const n = parseFloat(this.state.n);
+        var result = pv * Math.pow((1 +  r), n);
+        this.setState({fv: parseFloat(result).toFixed(4)});
         event.preventDefault();
     }
     
     render() {
         return(
                 <div className="CalculatorPV">
+                    <h1>Present Value to Future Value</h1>
+                    <br /><br />
                     <label>
                         Present Value:
                         <input type="number"
                                step="any"
                                min="0"
                                name="PV" 
-                               value={this.props.pv} 
+                               value={this.state.pv} 
                                onChange={this.pv_change} 
                         />
                     </label>
+                    <br /><br />
                     <label>
                         Number of Period:
                         <input type="number" 
                                name="n"
                                step="1"
                                min="0"
-                               value={this.props.n} 
+                               value={this.state.n} 
                                onChange={this.n_change} 
                         />
                     </label>
+                    <br /><br />
                     <label>
                         Interest Rate:
                         <input type="number" 
                                name="rate"
                                step="any"
-                               value={this.props.r} 
+                               value={this.state.r} 
                                onChange={this.r_change} 
                         />
                     </label>
@@ -118,8 +113,8 @@ class CalculatorPV extends Component {
                     <button type="button" onClick={this.PV_Submit}>
                         Calculate FV
                     </button>
-                                   
-                    <span>Future Value: {this.props.fv}</span>
+                    <br /><br />               
+                    <span>Future Value: {this.state.fv}</span>
                 </div>
                );       
     }    
@@ -128,6 +123,12 @@ class CalculatorPV extends Component {
 class CalculatorAV extends Component {
     constructor() {
         super();
+        this.state = {
+            av: 0.0,
+            n: 0,
+            r: 0.0,
+            fv: 0.0,
+        };
 
         this.av_change = this.av_change.bind(this);
         this.n_change = this.n_change.bind(this);
@@ -136,51 +137,59 @@ class CalculatorAV extends Component {
     }
     
     av_change(event) {        
-        this.props.handle_av(event.target.value);
+        this.setState({av: event.target.value});
     }
 
     n_change(event) {
-        this.props.handle_n(event.target.value);
+       this.setState({n: event.target.value});
     }
     
     r_change(event) {
-        this.props.handle_r(event.target.value);
+        this.setState({r: event.target.value});
     }
     
     AV_Submit(event) {
-        this.props.AV_Submit();
+        const av = parseFloat(this.state.av);
+        const r = parseFloat(this.state.r);
+        const n = parseFloat(this.state.n);
+        var result = av * ((Math.pow((1 +  r), n) - 1) / r);
+        this.setState({fv: parseFloat(result).toFixed(4)});
         event.preventDefault();
     }
     
     render() {
         return(
                 <div className="CalculatorPV">
+                    <h1>Annuity to Future Value</h1>
+                    <br /><br />
                     <label>
                         Annuity Value:
                         <input type="number"
                                step="any"
                                min="0"
                                name="AV" 
-                               value={this.props.av} 
+                               value={this.state.av} 
                                onChange={this.av_change} 
                         />
                     </label>
+                    <br /><br />
                     <label>
                         Number of Period:
                         <input type="number" 
                                name="n"
                                step="1"
                                min="0"
-                               value={this.props.n} 
+                               value={this.state.n} 
                                onChange={this.n_change} 
                         />
                     </label>
+                    <br /><br />
                     <label>
                         Interest Rate:
                         <input type="number" 
                                name="rate"
                                step="any"
-                               value={this.props.r} 
+                               value={this.state.r} 
                                onChange={this.r_change} 
                         />
                     </label>
@@ -188,8 +197,8 @@ class CalculatorAV extends Component {
                     <button type="button" onClick={this.AV_Submit}>
                         Calculate FV
                     </button>
-                                   
-                    <span>Future Value: {this.props.fv}</span>
+                    <br /><br />     
+                    <span>Future Value: {this.state.fv}</span>
                 </div>
                );       
     }    
@@ -200,98 +209,35 @@ class App extends Component {
         super(props);
         this.state = {
             choice: '',
-            pv: 0.0,
-            av: 0.0,
-            n: 0,
-            r: 0.0,
-            fv: 0.0,
         };
         
         this.click_PV = this.click_PV.bind(this);
-        this.click_AV = this.click_AV.bind(this);
-        this.handle_pv = this.handle_pv.bind(this);
-        this.handle_av = this.handle_av.bind(this);
-        this.handle_n = this.handle_n.bind(this);
-        this.handle_r = this.handle_r.bind(this);
-        this.PV_Submit = this.PV_Submit.bind(this);
-        this.AV_Submit = this.AV_Submit.bind(this);
+        this.click_AV = this.click_AV.bind(this);        
     }
     
     click_PV() {
         this.setState({
             choice: 'PV',
-            pv: 0.0,
-            av: 0.0,
-            n: 0,
-            r: 0.0,
-            fv: 0.0,
         });
     }
     
     click_AV() {
         this.setState({
             choice: 'AV',
-            pv: 0.0,
-            av: 0.0,
-            n: 0,
-            r: 0.0,
-            fv: 0.0,
         });
     }
-    
-    handle_pv(data) {        
-        this.setState({pv: data});
-    }
-    
-    handle_av(data) {        
-        this.setState({av: data});
-    }
-
-    handle_n(data) {
-        this.setState({n: data});
-    }
-    
-    handle_r(data) {
-        this.setState({r: data});
-    }
-    
-    PV_Submit() {
-        const pv = parseFloat(this.state.pv);
-        const r = parseFloat(this.state.r);
-        const n = parseFloat(this.state.n);
-        var result = pv * Math.pow((1 +  r), n);
-        this.setState({fv: parseFloat(result).toFixed(4)});
-    }
-    
-    AV_Submit() {
-        const av = parseFloat(this.state.av);
-        const r = parseFloat(this.state.r);
-        const n = parseFloat(this.state.n);
-        var result = av * ((Math.pow((1 +  r), n) - 1) / r);
-        this.setState({fv: parseFloat(result).toFixed(4)});
-    }
-    
+        
     render() {
         return (
                 <div className="App">
                     <HeaderBar 
+                        choice={this.state.choice}
                         click_PV={this.click_PV}
                         click_AV={this.click_AV}
                     />
                     <div className="Display">
                         <Calculator
-                            choice={this.state.choice}
-                            pv={this.state.pv}
-                            av={this.state.av}
-                            n={this.state.n}
-                            r={this.state.r}
-                            fv={this.state.fv}
-                            handle_pv={this.handle_pv}
-                            handle_av={this.handle_av}
-                            handle_n={this.handle_n}
-                            handle_r={this.handle_r}
-                            PV_Submit={this.PV_Submit}
-                            AV_Submit={this.AV_Submit}
+                            choice={this.state.choice}                            
                         />
                     </div>
                 </div>
